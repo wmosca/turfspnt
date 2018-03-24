68fa565e738aa048c4e11ac228d3f5ba4cb9b3f2
Connor Zapfel <connorzapfel@gmail.com>
2018-03-24 18:43:43 -0400

    Added the first activity meme

---
+++ app/src/main/java/com/willconjo/turfspnt/TurfLoginPage.java
@@ -0,0 +1,13 @@
+package com.willconjo.turfspnt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TurfLoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turf_login_page);
+    }
+}
