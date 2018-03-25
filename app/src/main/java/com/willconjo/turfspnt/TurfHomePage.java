ee00d7b2ad91a4a012eac033c6f1f24fbaca615e
Connor Zapfel <connorzapfel@gmail.com>
2018-03-25 08:33:29 -0400

    did stuff

--- app/src/main/java/com/willconjo/turfspnt/TurfHomePage.java
+++ app/src/main/java/com/willconjo/turfspnt/TurfHomePage.java
@@ -1,8 +1,10 @@
 package com.willconjo.turfspnt;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.view.View;
 
 public class TurfHomePage extends AppCompatActivity {
 
@@ -10,8 +12,16 @@ public class TurfHomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turf_home_page_empty);
-        Intent intent = new Intent(TurfHomePage.this, TurfWidgetService.class);
-        startService(intent);
+
+        FloatingActionButton addAccountButton = (FloatingActionButton)findViewById(R.id.addAccountButton);
+        addAccountButton.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent = new Intent(TurfHomePage.this, TurfAddAccountPage.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 
 }
