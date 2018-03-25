99b0d47a7e1b2f033e02459c8e85e354edc1349b
Connor Zapfel <connorzapfel@gmail.com>
2018-03-25 00:58:33 -0400

    removed setup db

--- application.py
+++ application.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template, request
+import sqlite3
+import os
 
 app = Flask(__name__)
 
 #First route
 @app.route('/', methods = ['POST','GET'])
 def index():
-	string = "HELLO WORLD"
-	return(string)
+    string = "HELLO WORLD"
+    return(string)
+
+@app.route("/login", methods=['POST'])
+def login():
+    login_form = request.form
+    print("received: {}".format(login_form))
 
 #Second route
 @app.route('/next', methods = ['POST','GET'])
 def cool():
-	newString = "HEHEHEHE"
-	return(newString)
+    newString = "HEHEHEHE"
+    return(newString)
 	
 if __name__=="__main__":
-	app.run()
+    app.run(host="155.246.213.67", port=55555)
